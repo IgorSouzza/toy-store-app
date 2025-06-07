4c1940137c545606fbab2704f9e3452b0ccf925e
Igor Souza <igor.souza.webmaster@gmail.com>
2025-06-07 12:09:02 -0300

    feat: add clients table

---
+++ src/features/dashboard/home/components/clients-table.tsx
@@ -0,0 +1,35 @@
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/shared/components/ui/table";
+
+export function ClientsTable() {
+  return (
+    <div className="px-4 lg:px-6">
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">Nome</TableHead>
+            <TableHead>E-mail</TableHead>
+            <TableHead>Data de nascimento</TableHead>
+            <TableHead>Alfabeto</TableHead>
+            <TableHead className="text-right">Total vendas</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell className="font-medium">INV001</TableCell>
+            <TableCell>Paid</TableCell>
+            <TableCell>Credit Card</TableCell>
+            <TableCell>ABCDEFGHIJKLMNOPQRSTUVXYZ</TableCell>
+            <TableCell className="text-right">$250.00</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </div>
+  );
+}
--- src/features/dashboard/home/page.tsx
+++ src/features/dashboard/home/page.tsx
@@ -3,6 +3,8 @@ import { AppSidebar } from "@/shared/components/sidebar";
 import { SiteHeader } from "@/shared/components/site-header";
 import { SidebarInset, SidebarProvider } from "@/shared/components/ui/sidebar";
 import { SalesPerDayChart } from "./components/sales-per-day-chart";
+import { ClientsTable } from "./components/clients-table";
+import { Button } from "@/shared/components/ui/button";
 
 export function DashboardHomePage() {
   return (
@@ -18,12 +20,16 @@ export function DashboardHomePage() {
       <SidebarInset>
         <SiteHeader />
         <div className="flex flex-1 flex-col">
-          <div className="@container/main flex flex-1 flex-col gap-2">
-            <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+          <div className="@container/main flex flex-1 flex-col gap-2 space-y-4">
+            <div className="flex flex-col gap-4 mt-4 md:mt-6 md:gap-6">
               <SectionCards />
             </div>
-            <div className="flex flex-col gap-4 py-4 md:gap-6">
+            <div className="flex flex-col md:gap-6">
               <SalesPerDayChart />
+            </div>
+            <div className="flex flex-col md:gap-6">
+              <Button className="w-fit mx-6 self-end">Adicionar cliente</Button>
+              <ClientsTable />
             </div>
           </div>
         </div>
